6378ea0e7415bf2ccb3ca13c00ed94dc789931cc
ZlidnyaMari <78682991+ZlidnyaMari@users.noreply.github.com>
2022-02-13 21:54:35 +0300

    React1

---
+++ gb-react-project/src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+import Message from './Message.js';
+
+
+function App() {
+
+  const message = 'hello world'
+  return (
+    <div className="App">
+        <Message />
+    </div>
+  );
+}
+
+export default App;
