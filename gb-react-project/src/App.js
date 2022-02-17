e2b037ca24f22d40664dcfce7ed4d0deae3053da
ZlidnyaMari <78682991+ZlidnyaMari@users.noreply.github.com>
2022-02-17 10:57:22 +0300

    Lesson2-1

--- gb-react-project/src/App.js
+++ gb-react-project/src/App.js
@@ -6,49 +6,49 @@ import { AUTORS } from './constant/constant';
 
 
 function App() {
-  const [ messegeList, setMessegeList ] = useState([]);
-  const [ value, setValue ] = useState( '' );
- 
+  const [messegeList, setMessegeList] = useState([]);
+  const [value, setValue] = useState('');
+
   let textHandler = (event) => {
     setValue(event.target.value);
   }
 
-  let handler = () =>{
-    if(value != '') {
-  setMessegeList( [...messegeList, {
-    autor: AUTORS.me,
-    text: value,
-  }]);
-  
-  setValue('');
-  }
-}
+  let handler = () => {
+    if (value != '') {
+      setMessegeList([...messegeList, {
+        autor: AUTORS.me,
+        text: value,
+      }]);
 
-useEffect( () => {
-  if(messegeList.length > 0 && messegeList[messegeList.length -1].autor === AUTORS.me) {
-    setMessegeList( [...messegeList, {
-      
-      autor: AUTORS.bot,
-      text: 'Привет, я бот'
-
-    }])
+      setValue('');
+    }
   }
-}, [messegeList])
+
+  useEffect(() => {
+    if (messegeList.length > 0 && messegeList[messegeList.length - 1].autor === AUTORS.me) {
+      setTimeout( () => setMessegeList([...messegeList, {
+
+        autor: AUTORS.bot,
+        text: 'Привет, я бот'
+
+      }]), 1500) 
+    }
+  }, [messegeList])
 
   return (
     <div className='container'>
-      <textarea className='messeng' value = {value} type = 'text' placeholder='Введите сообщение' onChange={textHandler}  ></textarea>
+      <textarea className='messeng' value={value} type='text' placeholder='Введите сообщение' onChange={textHandler}  ></textarea>
       <button className='button' onClick={handler}>Отправить</button>
-    
-    <ul className='list'>
-       {messegeList.map((item) => (
-      <>
-        <li className='listAutor'>{item.autor}</li>
-        <li className='listText'>{item.text}</li>
-      </>
-       ))}
-    </ul>
-      
+
+      <ul className='list'>
+        {messegeList.map((item) => (
+          <>
+            <li className='listAutor'>{item.autor}</li>
+            <li className='listText'>{item.text}</li>
+          </>
+        ))}
+      </ul>
+
     </div>
   );
 }
